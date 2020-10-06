27b78071d9beb54e98036a243b45f0f27d52b7f0
Iker <iiparraguirrepdaw1@gmail.com>
2020-10-06 13:18:24 +0200

    Iker Iparraguirre

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -20,6 +20,8 @@ public class TestEstudiante
         e3.printEstudiante();
         Estudiante e4 = new Estudiante("Roberto", 21);
         e4.printEstudiante();
+        Estudiante e5 = new Estudiante("Iker", 19);
+        e5.printEstudiante();
 
     }
 }
